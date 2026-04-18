d911bf1920f61f677dc1d670ca1f1dd040a3bee6
K. J. B. Thilakarathna <168623163+KasunJanith@users.noreply.github.com>
2026-04-18 11:05:27 +0530

    cleared the code

--- quick-fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Fixing contact page error...
-echo.
-taskkill /F /IM node.exe 2>nul >nul
-if exist .next rmdir /s /q .next
-echo Cache cleared! Starting server...
-npm run dev
--- restart-dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Clearing Next.js cache...
-if exist .next rmdir /s /q .next
-echo Cache cleared!
-echo.
-echo Starting development server...
-npm run dev
